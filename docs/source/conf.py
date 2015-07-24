d65a4345f431064acd77b3a3db874b413c95a9cb
stvhoey <stvhoey.vanhoey@ugent.be>
2015-07-24 22:30:58 +0200

    solved end of line issue

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -12,11 +12,12 @@
 # serve to show the default.
 
 import sys, os
+import sphinx_bootstrap_theme
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
-sys.path.insert(0, os.path.abspath('D:\Modellen\Version2012\Main\Model\Lumped'))
+sys.path.insert(0, os.path.abspath('D:\Projecten\Githubs'))
 
 # -- General configuration -----------------------------------------------------
 
@@ -91,7 +92,7 @@ pygments_style = 'sphinx'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'default'
+html_theme = 'bootstrap'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
@@ -99,7 +100,7 @@ html_theme = 'default'
 #html_theme_options = {}
 
 # Add any paths that contain custom themes here, relative to this directory.
-#html_theme_path = []
+html_theme_path = sphinx_bootstrap_theme.get_html_theme_path()
 
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
