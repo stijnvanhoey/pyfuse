2919d68ace5457fb22183095d541263c3f3e8169
stvhoey <stvhoey.vanhoey@ugent.be>
2016-03-14 14:35:40 +0100

    adapted numpy autosummary

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -38,6 +38,10 @@ extensions = [
     'numpydoc'
 ]
 
+# not automatically make a summary list of all class members
+# which causes a bunch of warnings
+numpydoc_show_class_members = False
+
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
